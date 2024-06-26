66b2041840c59abd8d271bab7b1d7422ec85179a
malldonado <matheussilvacontat@gmail.com>
2024-06-26 17:22:32 -0300

    update project

--- database/videotube.sql
+++ database/videotube.sql
@@ -1,27 +1,147 @@
--- --------------------------------------------------------
--- Servidor:                     127.0.0.1
--- Versão do servidor:           10.4.32-MariaDB - mariadb.org binary distribution
--- OS do Servidor:               Win64
--- HeidiSQL Versão:              12.6.0.6765
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 26, 2024 at 12:14 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- Exportação de dados foi desmarcado.
+--
+-- Database: `videotube`
+--
 
--- Exportação de dados foi desmarcado.
+-- --------------------------------------------------------
 
--- Exportação de dados foi desmarcado.
+--
+-- Table structure for table `categories`
+--
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Film & Animation'),
+(2, 'Autos & Vehicles'),
+(3, 'Music'),
+(4, 'Pets & Animals'),
+(5, 'Sports'),
+(6, 'Travel & Events'),
+(7, 'Gaming'),
+(8, 'People & Blogs'),
+(9, 'Comedy'),
+(10, 'Entertainment'),
+(11, 'News & Politics'),
+(12, 'Howto & Style'),
+(13, 'Education'),
+(14, 'Science & Technology'),
+(15, 'Nonprofits & Activism');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `thumbnails`
+--
+
+CREATE TABLE `thumbnails` (
+  `id` int(11) NOT NULL,
+  `videoId` int(11) NOT NULL,
+  `filePath` int(250) NOT NULL,
+  `selected` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `videos`
+--
+
+CREATE TABLE `videos` (
+  `id` int(11) NOT NULL,
+  `uploadedBy` varchar(50) NOT NULL,
+  `title` varchar(70) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `privacy` int(11) NOT NULL DEFAULT 0,
+  `filePath` varchar(250) NOT NULL,
+  `category` int(11) NOT NULL DEFAULT 0,
+  `uploadDate` datetime NOT NULL,
+  `views` int(11) NOT NULL DEFAULT 0,
+  `duration` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `videos`
+--
+
+INSERT INTO `videos` (`id`, `uploadedBy`, `title`, `description`, `privacy`, `filePath`, `category`, `uploadDate`, `views`, `duration`) VALUES
+(1, 'REPLACE-THIS', 'fgafafa', 'fafafaf', 0, 'uploads/videos/667b3e55148de.mp4', 1, '0000-00-00 00:00:00', 0, ''),
+(2, 'REPLACE-THIS', 'fgafafa', 'fafafaf', 0, 'uploads/videos/667b3f18a5a0e.mp4', 1, '0000-00-00 00:00:00', 0, ''),
+(3, 'REPLACE-THIS', 'fgafafa', 'fafafaf', 0, 'uploads/videos/667b3f2d77a0c.mp4', 1, '0000-00-00 00:00:00', 0, ''),
+(4, 'REPLACE-THIS', 'fgafafa', 'agafafgaga', 0, 'uploads/videos/667b3fa4be164.mp4', 1, '0000-00-00 00:00:00', 0, '00:50'),
+(5, 'REPLACE-THIS', 'fafffffff', 'ffffffff', 0, 'uploads/videos/667b40242549d.mp4', 1, '0000-00-00 00:00:00', 0, '00:50'),
+(6, 'REPLACE-THIS', 'fafffffff', 'ffffffff', 0, 'uploads/videos/667b40605e4f4.mp4', 1, '0000-00-00 00:00:00', 0, '00:50');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `thumbnails`
+--
+ALTER TABLE `thumbnails`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `thumbnails`
+--
+ALTER TABLE `thumbnails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `videos`
+--
+ALTER TABLE `videos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ watch.php
@@ -0,0 +1,29 @@
+<?php
+require_once("includes/header.php");
+require_once("includes/classes/VideoPlayer.php");
+require_once("includes/classes/VideoInfoSection.php");
+if (!isset($_GET["id"])) {
+    echo 'No url passed into page';
+    exit();
+}
+
+$video = new Video($con, $_GET["id"], $userLoggedInObj);
+$video->incrementViews();
+
+?>
+
+<div class="watchLeftColumn">
+<?php 
+
+$videoPlayer = new VideoPlayer($video);
+echo $videoPlayer->create(true);
+
+$videoPlayer = new VideoInfoSection($con, $video, $userLoggedInObj);
+echo $videoPlayer->create();
+
+?>
+</div>
+<div class="suggestions">
+
+</div>
+<?php require_once("includes/footer.php"); ?>
